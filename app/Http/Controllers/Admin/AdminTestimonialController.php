fe16b11896bbee5681b20bc12fd4ebdcd5fc43e8
maulana677 <maull0776@gmail.com>
2023-05-24 09:47:57 +0700

    testimonial section 3 commit

--- app/Http/Controllers/Admin/AdminTestimonialController.php
+++ app/Http/Controllers/Admin/AdminTestimonialController.php
@@ -43,4 +43,51 @@ class AdminTestimonialController extends Controller
         return redirect()->route('admin_testimonial_show')->with('success', 'Data is inserted successfully.');
     }
 
+    public function edit($id)
+    {
+        $row_data = Testimonial::where('id',$id)->first();
+        return view('admin.testimonial_edit', compact('row_data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'designation' => 'required',
+            'comment' => 'required',
+        ]);
+
+        $obj = Testimonial::where('id', $id)->first();
+
+        if ($request->hasFile('photo')) {
+            $request->validate([
+                'photo' => 'image|mimes:jpg,jpeg,png,gif'
+            ]);
+            unlink(public_path('uploads/'.$obj->photo));
+
+            $ext = $request->file('photo')->extension();
+            $final_name = 'testimonial_'.time().'.'.$ext;
+
+            $request->file('photo')->move(public_path('uploads/'),$final_name);
+
+            $obj->photo = $final_name;
+        }
+
+        $obj->name = $request->name;
+        $obj->designation = $request->designation;
+        $obj->comment = $request->comment;
+        $obj->update();
+
+        return redirect()->route('admin_testimonial_show')->with('success', 'Data is updated successfully.');
+    }
+
+    public function delete($id)
+    {
+        $row_data = Testimonial::where('id',$id)->first();
+        unlink(public_path('uploads/'.$row_data->photo));
+        $row_data->delete();
+
+        return redirect()->back()->with('success', 'Data is deleted successfully.');
+    }
+
 }
