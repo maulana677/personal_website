771c732a4c152da782388615c9763dc9d9a974c7
maulana677 <maull0776@gmail.com>
2023-05-27 20:39:03 +0700

    service section 7 commit

--- app/Http/Controllers/Front/ServiceController.php
+++ app/Http/Controllers/Front/ServiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\PageItem;
 use App\Models\Service;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,7 @@ class ServiceController extends Controller
     public function index()
     {
         $services = Service::orderBy('item_order', 'asc')->get();
-        return view('frontend.services', compact('services'));
+        $page_data = PageItem::where('id', 1)->first();
+        return view('frontend.services', compact('services','page_data'));
     }
 }
--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -1,11 +1,11 @@
 @extends('frontend.layout.app')
 
 @section('main_content')
-<div class="page-banner" style="background-image: url({{ asset('dist_front/images/page-banner.jpg') }})">
+<div class="page-banner" style="background-image: url({{ asset('uploads/'.$page_data->services_banner) }})">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1>Services</h1>
+                <h1>{{ $page_data->services_heading }}</h1>
             </div>
         </div>
     </div>
