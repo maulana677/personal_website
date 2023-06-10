17bb56043d74a1704725330a8ae7edb3b371cd16
maulana677 <maull0776@gmail.com>
2023-06-10 15:34:21 +0700

    blog part 3

--- app/Http/Controllers/Admin/AdminPostCategoryController.php
+++ app/Http/Controllers/Admin/AdminPostCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\PostCategory;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class AdminPostCategoryController extends Controller
 {
@@ -23,7 +24,7 @@ class AdminPostCategoryController extends Controller
     {
         $request->validate([
             'category_name' => 'required',
-            'category_slug' => 'required|unique:post_categories'
+            'category_slug' => 'required|alpha_dash|unique:post_categories'
         ]);
 
         $obj = new PostCategory();
@@ -37,34 +38,30 @@ class AdminPostCategoryController extends Controller
 
     public function edit($id)
     {
-        $row_data = PortfolioCategory::where('id',$id)->first();
-        return view('admin.portfolio_category_edit', compact('row_data'));
+        $row_data = PostCategory::where('id',$id)->first();
+        return view('admin.post_category_edit', compact('row_data'));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
-            'category_name' => 'required'
+            'category_name' => 'required',
+            'category_slug' => ['required', 'alpha_dash', Rule::unique('post_categories')->ignore($id)],
         ]);
 
-        $obj = PortfolioCategory::where('id', $id)->first();
+        $obj = PostCategory::where('id', $id)->first();
 
         $obj->category_name = $request->category_name;
+        $obj->category_slug = $request->category_slug;
         $obj->update();
 
-        return redirect()->route('admin_portfolio_category_show')->with('success', 'Data is updated successfully.');
+        return redirect()->route('admin_post_category_show')->with('success', 'Data is updated successfully.');
     }
 
     public function delete($id)
     {
-        $row_data = PortfolioCategory::where('id',$id)->first();
-
-        $count = Portfolio::where('portfolio_category_id', $id)->count();
-        if($count==0) {
-            $row_data->delete();
-        } else {
-            return redirect()->back()->with('error', 'You can delete this portfolio category, because there is one or more portfolio under this.');
-        }
+        $row_data = PostCategory::where('id',$id)->first();
+        $row_data->delete();
 
         return redirect()->back()->with('success', 'Data is deleted successfully.');
     }
